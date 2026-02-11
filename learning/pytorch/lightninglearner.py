ee2e09b947b7b6dca9ef335bc5cfc9064fec44c1
ShrestaG <136068815+ShrestaG@users.noreply.github.com>
2026-02-11 13:26:33 +0530

    Add files via upload

---
+++ learning/pytorch/lightninglearner.py
@@ -0,0 +1,193 @@
+# 
+# This file an adaptation and extension of the p2pfl library (https://pypi.org/project/p2pfl/).
+# Refer to the LICENSE file for licensing information.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import logging
+import pickle
+import time
+from collections import OrderedDict
+
+import torch
+from lightning import Trainer
+from lightning.pytorch.callbacks import ModelSummary
+from lightning.pytorch.callbacks import RichProgressBar, RichModelSummary
+from lightning.pytorch.callbacks.progress.rich_progress import RichProgressBarTheme
+
+from fedstellar.learning.exceptions import DecodingParamsError, ModelNotMatchingError
+from fedstellar.learning.learner import NodeLearner
+
+
+###########################
+#    LightningLearner     #
+###########################
+
+
+class LightningLearner(NodeLearner):
+    """
+    Learner with PyTorch Lightning.
+
+    Atributes:
+        model: Model to train.
+        data: Data to train the model.
+        epochs: Number of epochs to train.
+        logger: Logger.
+    """
+
+    def __init__(self, model, data, config=None, logger=None):
+        self.model = model
+        # self.model = torch.compile(model)  # PyTorch 2.0
+        self.data = data
+        self.config = config
+        self.logger = logger
+        self.__trainer = None
+        self.epochs = 1
+        logging.getLogger("lightning.pytorch").setLevel(logging.WARNING)
+
+        # FL information
+        self.round = 0
+        # self.local_step = 0
+        # self.global_step = 0
+
+        self.logger.log_metrics({"Round": self.round}, step=self.logger.global_step)
+
+
+    def set_model(self, model):
+        self.model = model
+
+    def set_data(self, data):
+        self.data = data
+
+    def encode_parameters(self, params=None, contributors=None, weight=None):
+        if params is None:
+            params = self.model.state_dict()
+        array = [val.cpu().numpy() for _, val in params.items()]
+        return pickle.dumps((array, contributors, weight))
+
+    def decode_parameters(self, data):
+        try:
+            params, contributors, weight = pickle.loads(data)
+            params_dict = zip(self.model.state_dict().keys(), params)
+            return (
+                OrderedDict({k: torch.tensor(v) for k, v in params_dict}),
+                contributors,
+                weight,
+            )
+        except DecodingParamsError:
+            raise DecodingParamsError("Error decoding parameters")
+
+    def check_parameters(self, params):
+        # Check ordered dict keys
+        if set(params.keys()) != set(self.model.state_dict().keys()):
+            return False
+        # Check tensor shapes
+        for key, value in params.items():
+            if value.shape != self.model.state_dict()[key].shape:
+                return False
+        return True
+
+    def set_parameters(self, params):
+        try:
+            self.model.load_state_dict(params)
+        except ModelNotMatchingError:
+            raise ModelNotMatchingError("Not matching models")
+
+    def get_parameters(self):
+        return self.model.state_dict()
+
+    def set_epochs(self, epochs):
+        self.epochs = epochs
+
+    def fit(self):
+        try:
+            if self.epochs > 0:
+                self.create_trainer()
+                self.__trainer.fit(self.model, self.data)
+                self.__trainer = None
+        except Exception as e:
+            logging.error("Something went wrong with pytorch lightning. {}".format(e))
+
+    def interrupt_fit(self):
+        if self.__trainer is not None:
+            self.__trainer.should_stop = True
+            self.__trainer = None
+
+    def evaluate(self):
+        try:
+            if self.epochs > 0:
+                self.create_trainer()
+                self.__trainer.test(self.model, self.data, verbose=True)
+                self.__trainer = None
+                # results = self.__trainer.test(self.model, self.data, verbose=True)
+                # loss = results[0]["Test/Loss"]
+                # metric = results[0]["Test/Accuracy"]
+                # self.__trainer = None
+                # self.log_validation_metrics(loss, metric, self.round)
+                # return loss, metric
+            else:
+                return None
+        except Exception as e:
+            logging.error("Something went wrong with pytorch lightning. {}".format(e))
+            return None
+
+    def log_validation_metrics(self, loss, metric, round=None, name=None):
+        self.logger.log_metrics({"Test/Loss": loss, "Test/Accuracy": metric}, step=self.logger.global_step)
+        pass
+
+    def get_num_samples(self):
+        return (
+            len(self.data.train_dataloader().dataset),
+            len(self.data.test_dataloader().dataset),
+        )
+
+    def init(self):
+        self.close()
+
+    def close(self):
+        if self.logger is not None:
+            pass
+
+    def finalize_round(self):
+        self.logger.global_step = self.logger.global_step + self.logger.local_step
+        self.logger.local_step = 0
+        pass
+
+    def create_trainer(self):
+        logging.info("[Learner] Creating trainer with accelerator: {}".format(self.config.participant["device_args"]["accelerator"]))
+        progress_bar = RichProgressBar(
+            theme=RichProgressBarTheme(
+                description="green_yellow",
+                progress_bar="green1",
+                progress_bar_finished="green1",
+                progress_bar_pulse="#6206E0",
+                batch_progress="green_yellow",
+                time="grey82",
+                processing_speed="grey82",
+                metrics="grey82",
+            ),
+            leave=True,
+        )
+        self.__trainer = Trainer(
+            callbacks=[RichModelSummary(max_depth=1), progress_bar],
+            max_epochs=self.epochs,
+            accelerator=self.config.participant["device_args"]["accelerator"],
+            devices="auto",
+            # strategy=self.config.participant["device_args"]["strategy"] if self.config.participant["device_args"]["accelerator"] != "auto" else None,
+            logger=self.logger,
+            log_every_n_steps=20,
+            enable_checkpointing=False,
+            enable_model_summary=False,
+            enable_progress_bar=True,
+        )
